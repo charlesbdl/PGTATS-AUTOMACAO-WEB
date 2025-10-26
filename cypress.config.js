d2c04887317247dccddaa9dd1526338559f500f5
charlesbdl <charlesbarrosdaluz@gmail.com>
2025-10-26 15:40:32 -0300

    trabalhando em pipeline de execução de testes

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,15 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.{js,ts,jsx,tsx}",
     supportFile: "cypress/support/e2e.js",
+    
+    // Configuração para relatórios JUnit (XML para CI/CD)
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'cypress/reports/junit/results-[hash].xml',
+      toConsole: true,
+      attachments: true,
+      testCaseSwitchClassnameAndName: false
+    }
   },
 
   component: {
